9e6d731988eb85af75c5edc0ce88408475be290e
esamrezk4 <92589088+esamrezk4@users.noreply.github.com>
2023-07-19 22:01:24 +0300

    built citiesContext

---
+++ src/contexts/CitiesContext.jsx
@@ -0,0 +1,38 @@
+const BASE_URL = "http://localhost:8000";
+import { createContext, useState, useEffect } from "react";
+
+const CitiesContext = createContext();
+
+function CitiesProvider({ children }) {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(function () {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+
+        setCities(data);
+      } catch {
+        alert("there was an error while loading data");
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchCities();
+  }, []);
+
+  return (
+    <CitiesContext.Provider
+      value={{
+        cities,
+        isLoading,
+      }}
+    >
+      {children}
+    </CitiesContext.Provider>
+  );
+}
+export { CitiesContext, CitiesProvider };
